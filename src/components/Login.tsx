46355c795d784538849d22640b4f1d7d4a87ed31
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-10-17 13:38:02 +0200

    Update Login.tsx
    Removed some consoloe.logs

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,51 +1,53 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { log } from 'console';
-
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { log } from "console";
 
 const Login = () => {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const router = useRouter();
-    const supabase = createClientComponentClient();
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
+  const handleSignIn = async () => {
+    const { data: session, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
 
-    const handleSignIn = async () => {
-      const {data: session, error} = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      });
+    if (session) {
+      router.push("admin");
+    }
 
-      if(session) {
-        router.push('admin')
-      }
+    if (error) {
+      console.log(error);
+    }
 
-      if(error) {
-        console.log(error);
-        
-      }
-
-
-      router.refresh();
-    };
-    
-    // console.log(email);
-    // console.log(password);
-    
+    router.refresh();
+  };
 
   return (
     <div>
-        <form>
-            <input type="email" placeholder='email' onChange={e => setEmail(e.target.value)} value={email}/>
-            <input type="password" placeholder='password' onChange={e => setPassword(e.target.value)} value={password}/>
-        </form>
-        <button onClick={handleSignIn}>Sign in</button>
+      <form>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </form>
+      <button onClick={handleSignIn}>Sign in</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
