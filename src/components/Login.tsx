1837ba2fecf06854e119d375248965b2f2adc8b8
Robin Persson <robinpn98@hotmail.se>
2023-10-17 12:55:44 +0200

    added admin login
    - Added a simple first iteration of admin login
    - needs to be updated and more secure

---
+++ src/components/Login.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { log } from 'console';
+
+
+const Login = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+    const router = useRouter();
+    const supabase = createClientComponentClient();
+
+
+    const handleSignIn = async () => {
+      const {data: session, error} = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if(session) {
+        router.push('admin')
+      }
+
+      if(error) {
+        console.log(error);
+        
+      }
+
+
+      router.refresh();
+    };
+    
+    // console.log(email);
+    // console.log(password);
+    
+
+  return (
+    <div>
+        <form>
+            <input type="email" placeholder='email' onChange={e => setEmail(e.target.value)} value={email}/>
+            <input type="password" placeholder='password' onChange={e => setPassword(e.target.value)} value={password}/>
+        </form>
+        <button onClick={handleSignIn}>Sign in</button>
+    </div>
+  )
+}
+
+export default Login
