d1408618878e71d58d3320ab569a68bcb74880bf
Robin Persson <robinpn98@hotmail.se>
2023-10-19 11:26:28 +0200

    admin login redirect
    Made it so if the user is logged in as admin he will be redirected to admin page instead of the login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import type { NextRequest } from 'next/server'
 
 const protectedRoute = ['/admin']
+const adminLoginRoute = ['/login']
 
 export default async function middleware(req: NextRequest) {
   const res = NextResponse.next()
@@ -12,6 +13,12 @@ export default async function middleware(req: NextRequest) {
   if (!cookie.data.session && protectedRoute.includes(req.nextUrl.pathname)) {
     const absoluteURL = new URL('/', req.nextUrl.origin)
     return NextResponse.redirect(absoluteURL.toString())
+  }
+
+  //If the user is logged in as admin he will be redirected to admin page instead of login
+  if (cookie.data.session && adminLoginRoute.includes(req.nextUrl.pathname)) {
+    const redirectToAdmin = new URL('/admin', req.nextUrl.origin)
+    return NextResponse.redirect(redirectToAdmin.toString())
   }
 
   return res
