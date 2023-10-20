25e695f5ea09dc11ff4e95d4ddf64d34a8a53628
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-10-20 15:08:58 +0200

    Update page.tsx
    The "om oss" page now successfully renders "body_text" from the "our_philosophy" table.

--- src/app/om_oss/page.tsx
+++ src/app/om_oss/page.tsx
@@ -1,11 +1,28 @@
 import styles from '../page.module.css'
 import NavBar from '@/components/NavBar/NavBar'
+import { Database } from '@/lib/codeBlockSupabase'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
-const page = () => {
+const page = async () => {
+  const cookiesStore = cookies()
+  const supabase = createServerComponentClient<Database>({
+    cookies: () => cookiesStore,
+  })
+
+  const { data: philosophies } = await supabase.from('our_philosophy').select()
+
+  philosophies?.map(philosophy => {
+    console.log(`philosophy: ${philosophy.body_text}`)
+  })
+
   return (
     <main className={styles.main}>
       <NavBar />
       <h1>OM OSS</h1>
+      {philosophies?.map(philosophy => (
+        <p>{philosophy.body_text}</p>
+      ))}
     </main>
   )
 }
