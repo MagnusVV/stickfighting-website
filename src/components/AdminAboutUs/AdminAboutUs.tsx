c1a2acac83c7ac1e15a80ec1872d711fd658b1f9
Robin Persson <robinpn98@hotmail.se>
2023-10-24 13:31:18 +0200

    fixed prop drilling for philosophy

--- src/components/AdminAboutUs/AdminAboutUs.tsx
+++ src/components/AdminAboutUs/AdminAboutUs.tsx
@@ -3,42 +3,46 @@ import React, { useEffect, useState } from 'react'
 import OurPhilosophy from './OurPhilosophy/OurPhilosophy'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/lib/codeBlockSupabase'
-
-interface fetchObj {
-  title: string
-  body_text: string
-}
+import fetchObj from '@/lib/types'
 
 const AdminAboutUs = () => {
-  const [about, setAbout] = useState<fetchObj | null>(null)
-  const [instructors, setInstructors] = useState<{}>()
-  const [philosophy, setPhilosophy] = useState<{}>()
+  const [about, setAbout] = useState<fetchObj>()
+  const [instructors, setInstructors] = useState<fetchObj>()
+  const [philosophy, setPhilosophy] = useState<fetchObj>()
   const supabase = createClientComponentClient<Database>()
 
   //fetch from the pivot table to get the specific information from the different tables
-  const handleMasterFetch = async () => {
-    let { data: about_page, error } = await supabase.from('about_page').select(`
-    about_association(title, body_text),
-    instructors(id, name, body_text),
-    our_philosophy(title, body_text)
-  `)
+  useEffect(() => {
+    const handleMasterFetch = async () => {
+      let { data, error } = await supabase.from('about_page').select(`
+        about_association(title, body_text),
+        instructors(id, name, body_text),
+        our_philosophy(title, body_text)
+      `)
 
-    if (error) {
-      console.log(error)
+      if (error) {
+        console.log(error)
+      }
+
+      if (data) {
+        //set the fetch data into states
+        setAbout(data[0]?.about_association as fetchObj)
+        setPhilosophy(data[0]?.our_philosophy as fetchObj)
+      }
     }
 
-    if (about_page) {
-      console.log(about_page)
-      setAbout(about_page[0]?.about_association as fetchObj)
-    }
-  }
+    handleMasterFetch()
+  }, [])
 
-  return (
+  // if Philosophy hasn't finished fetching data from supabase return a loading paragraph.
+  return !philosophy ? (
+    <p>Loading...</p>
+  ) : (
     <>
       <h1>AdminAboutUsComponent</h1>
-      <OurPhilosophy />
-      <button onClick={handleMasterFetch}>Fetch data</button>
+      <OurPhilosophy philosophy={philosophy} setPhilosophy={setPhilosophy} />
       <p>{about?.body_text}</p>
+      <p>{philosophy?.body_text}</p>
     </>
   )
 }
---
+++ src/lib/types.ts
@@ -0,0 +1,4 @@
+export default interface fetchObj {
+  title?: string
+  body_text: string
+}
