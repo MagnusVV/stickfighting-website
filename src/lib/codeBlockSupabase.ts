e0108c4294316a2c4fef35d8e9f0600030721b99
Robin Persson <robinpn98@hotmail.se>
2023-11-02 14:36:05 +0100

    minor changes and updated types from supabase

--- src/lib/codeBlockSupabase.ts
+++ src/lib/codeBlockSupabase.ts
@@ -35,6 +35,7 @@ export interface Database {
           {
             foreignKeyName: "about_association_profile_id_fkey"
             columns: ["profile_id"]
+            isOneToOne: false
             referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
@@ -63,19 +64,109 @@ export interface Database {
           {
             foreignKeyName: "about_page_about_id_fkey"
             columns: ["about_id"]
+            isOneToOne: false
             referencedRelation: "about_association"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "about_page_intructor_id_fkey"
             columns: ["intructor_id"]
+            isOneToOne: false
             referencedRelation: "instructors"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "about_page_philosophy_id_fkey"
             columns: ["philosophy_id"]
+            isOneToOne: false
             referencedRelation: "our_philosophy"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      editor_test: {
+        Row: {
+          body_text: Json | null
+          created_at: string
+          id: number
+          profile_id: string | null
+        }
+        Insert: {
+          body_text?: Json | null
+          created_at?: string
+          id?: number
+          profile_id?: string | null
+        }
+        Update: {
+          body_text?: Json | null
+          created_at?: string
+          id?: number
+          profile_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "editor_test_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      events: {
+        Row: {
+          cancelled: boolean
+          created_at: string
+          event_date: string
+          event_end_time: string
+          event_start_time: string
+          id: number
+          instructor_id: number | null
+          locale_id: number
+          profile_id: string
+        }
+        Insert: {
+          cancelled?: boolean
+          created_at?: string
+          event_date: string
+          event_end_time: string
+          event_start_time: string
+          id?: number
+          instructor_id?: number | null
+          locale_id: number
+          profile_id: string
+        }
+        Update: {
+          cancelled?: boolean
+          created_at?: string
+          event_date?: string
+          event_end_time?: string
+          event_start_time?: string
+          id?: number
+          instructor_id?: number | null
+          locale_id?: number
+          profile_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "events_instructor_id_fkey"
+            columns: ["instructor_id"]
+            isOneToOne: false
+            referencedRelation: "instructors"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "events_locale_id_fkey"
+            columns: ["locale_id"]
+            isOneToOne: false
+            referencedRelation: "locale"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "events_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
         ]
@@ -106,10 +197,29 @@ export interface Database {
           {
             foreignKeyName: "instructors_profile_id_fkey"
             columns: ["profile_id"]
+            isOneToOne: false
             referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
         ]
+      }
+      locale: {
+        Row: {
+          created_at: string
+          id: number
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          name: string
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          name?: string
+        }
+        Relationships: []
       }
       news: {
         Row: {
@@ -140,6 +250,7 @@ export interface Database {
           {
             foreignKeyName: "news_profile_id_fkey"
             columns: ["profile_id"]
+            isOneToOne: false
             referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
@@ -171,6 +282,7 @@ export interface Database {
           {
             foreignKeyName: "our_philosophy_profile_id_fkey"
             columns: ["profile_id"]
+            isOneToOne: false
             referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
@@ -205,6 +317,7 @@ export interface Database {
           {
             foreignKeyName: "profiles_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
           }
