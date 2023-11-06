c948e8f225cf1d31cba9d3eb2c5dcd04db717d3b
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-11-06 14:46:55 +0100

    Events are now fetched from Supabase.
    Also displayed in calendar.
    
    Added several new types.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,3 +10,23 @@ export interface InstructorParams {
 }
 
 export type InstructorCollection = InstructorParams[]
+
+// Fetched event format -MV
+export interface Event {
+  id: number
+  cancelled: boolean
+  event_date: string
+  event_start_time: string
+  event_end_time: string
+  instructors: { name: string | null } | null
+  locale: { name: string } | null
+}
+
+// Event formatted for calendar input -MV
+export interface FormattedEvent {
+  id: number
+  start: Date
+  end: Date
+  title: string
+  cancelled: boolean | null
+}
