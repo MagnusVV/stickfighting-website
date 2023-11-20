4f3414ee46debb08dab61cd0bc6cf5cd528f81f8
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-11-20 10:01:26 +0100

    Merged with Robin's news work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
 import styles from './page.module.css'
 import NavBar from '@/components/NavBar/NavBar'
 import useSupabaseServer from '@/lib/supabaseServer'
-import Bold from '@tiptap/extension-bold'
-import Italic from '@tiptap/extension-italic'
-import ListItem from '@tiptap/extension-list-item'
-import BulletList from '@tiptap/extension-bullet-list'
-// Option 2: Browser-only (lightweight)
-import { generateHTML } from '@tiptap/core'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import { generateText, useEditor } from '@tiptap/react'
-import { useMemo } from 'react'
-import parser from 'html-react-parser'
-import { JSONContent } from '@tiptap/react'
+
 import HomeNews from '@/components/homeNews/HomeNews'
 
 export default async function Home() {
   const { supabase } = useSupabaseServer()
-
-  const { data: news } = await supabase.from('news').select()
 
   // TODO: Move this to own componenet? -MV
   const WelcomeVideo = () => {
