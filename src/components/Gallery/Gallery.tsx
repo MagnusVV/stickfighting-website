dd53a83f603b8c67b3b51381b0301043f5c100a3
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-11-22 14:04:33 +0100

    Initial grid work

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -43,7 +43,7 @@ const Gallery = () => {
               quality={75}
               alt="Träningsträff"
               layout="fill"
-              objectFit="contain"
+              objectFit="cover"
             />
           </div>
         ) : null,
