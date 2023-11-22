1a17af60b6f98532f89d0c1e77921a062e52590c
Robin Persson <robinpn98@hotmail.se>
2023-11-22 13:37:29 +0100

    minor updates

--- src/components/AdminNews/EditNews/EditNews.tsx
+++ src/components/AdminNews/EditNews/EditNews.tsx
@@ -107,6 +107,7 @@ const EditNews: React.FC<newsProps> = ({
       >
         <label htmlFor="title">titel</label>
         <input
+          className={styles.input}
           id="title"
           name="title"
           type="text"
@@ -116,6 +117,7 @@ const EditNews: React.FC<newsProps> = ({
         />
         <label htmlFor="ingress">Ingress</label>
         <input
+          className={styles.input}
           id="ingress"
           name="ingress"
           type="text"
@@ -126,7 +128,7 @@ const EditNews: React.FC<newsProps> = ({
         <label htmlFor="body_text">Text</label>
         <MenuBar editor={editor} />
         <EditorContent editor={editor} />
-        <button type="submit">Updatera</button>
+        <Button text="Uppdatera" styling={genericButton} type="submit" />
       </form>
     </div>
   )
