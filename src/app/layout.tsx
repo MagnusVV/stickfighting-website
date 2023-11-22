6e423d63ce0f7c932f8acba3631b9240744c758c
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-11-22 18:52:48 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={work_sans.className}>
-        <PageTitle /> {children} <Footer />
+        {children} <Footer />
       </body>
     </html>
   )
